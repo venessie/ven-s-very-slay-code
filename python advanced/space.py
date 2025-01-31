7c50dedbd9054d0d64503610a8dcb4934df8b38e
venessie <venplayz527@gmail.com>
2025-01-31 11:09:24 +0800

    acp

--- python advanced/space.py
+++ python advanced/space.py
@@ -12,3 +12,22 @@ def arraysum(a):
 
 a = [1,1,1,1]
 print(arraysum(a))
+
+
+
+
+def myfunction(n):
+    for i in range(0,n+1):
+        print("First Loop")
+        
+    j=1
+    while(j<=n+1):
+        print("Second Loop", j)
+        j=j*2
+
+    for i in range(0,100):
+        print("Third Loop")
+
+
+print(myfunction(3))
+    
