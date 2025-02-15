8979e70dbfa8a7e0fd917fe723191d6afb57e19f
venessie <venplayz527@gmail.com>
2025-02-15 13:09:12 +0800

    saturday febuary 15th 2025

---
+++ python advanced/e.py
@@ -0,0 +1,30 @@
+from math import sqrt # Import sqrt function
+
+# Input from the user
+
+number = int(input("Enter your number: "))
+
+print("\n")
+
+# Check if the number is greater than 1
+
+if number > 1:
+
+    for i in range(2, int(sqrt(number)) + 1):
+
+        if number % i == 0:
+
+           print(number, "is not a prime number")
+
+           break # Exit loop if a factor is found
+
+    else:
+
+       print(number, "is a prime number") # Runs if loop completes without breaking
+
+else:
+
+
+    print(number, "is not a prime number") # Handles numbers <= 1
+
+ 
